ca13770a1b86961a0892f9c02f9952dbf61f43f0
Lee youngtaek <ting4602@gmail.com>
2016-09-21 22:19:11 +0900

    Add Project

---
+++ app/src/main/java/com/naxesa/bodyheat/MainActivity.java
@@ -0,0 +1,53 @@
+package com.naxesa.bodyheat;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    // Views
+    private Button btnBodyHeat, btnClock, btnNewsFeed, btnTip;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // View Reference
+        btnBodyHeat = (Button)findViewById(R.id.body_heat);
+        btnClock = (Button)findViewById(R.id.clock);
+        btnNewsFeed = (Button)findViewById(R.id.news_feed);
+        btnTip = (Button)findViewById(R.id.tip);
+
+        // View Event
+        btnBodyHeat.setOnClickListener(this);
+        btnClock.setOnClickListener(this);
+        btnNewsFeed.setOnClickListener(this);
+        btnTip.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        Intent intent = null;
+        switch (view.getId()){
+            case R.id.body_heat:
+                intent = new Intent(getApplicationContext(), BodyHeatActivity.class);
+                break;
+            case R.id.clock:
+                intent = new Intent(getApplicationContext(), ClockActivity.class);
+                break;
+            case R.id.news_feed:
+                intent = new Intent(getApplicationContext(), NewsFeedActivity.class);
+                break;
+            case R.id.tip:
+                intent = new Intent(getApplicationContext(), TipActivity.class);
+                break;
+            default:
+                break;
+        }
+        startActivity(intent);
+    }
+}
---
+++ app/src/main/java/com/naxesa/bodyheat/TipActivity.java
@@ -0,0 +1,16 @@
+package com.naxesa.bodyheat;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by DSM_055 on 2016-09-21.
+ */
+
+public class TipActivity extends Activity {
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tip);
+    }
+}
